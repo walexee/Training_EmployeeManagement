13f0a7faf420bf0f35f65b1178af459d50758328
Wale Adeleye <olawale.adeleye@gmail.com>
2016-11-21 15:44:57 +0100

    initial commit

---
+++ EmployeeManagement.Core/Models/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Core.Models
+{
+    public class Employee
+    {
+        private string _jobTitle;
+
+        public string Firstname { get; set; }
+
+        public int Id { get; private set; }
+
+        public string Lastname { get; set; }
+
+        public string JobTitle
+        {
+            get
+            {
+                return _jobTitle;
+            }
+            set
+            {
+                _jobTitle = value;
+            }
+        }
+
+        public int SkillLevel { get; set; }
+
+        public double Salary { get; set; }
+
+        public void TakeVacation(int hours)
+        {
+
+        }
+    }
+}
