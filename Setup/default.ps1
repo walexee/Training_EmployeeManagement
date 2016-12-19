4edbcaf0c5c332507aa482fed3918cd607d269dd
Olawale Adeleye <olawale.adeleye@gmail.com>
2016-12-19 07:51:13 -0500

    Added test runners

--- Setup/default.ps1
+++ Setup/default.ps1
@@ -1,4 +1,6 @@
-﻿properties {
+﻿Include ".\helpers.ps1"
+
+properties {
 	$testMessage = 'Executed Clean!'
 	$cleanMessage = 'Executed Clean!'
 
@@ -7,8 +9,22 @@
 	$temporaryOutputDirectory = "$outputDirectory\temp"
 	$newDirectoryList = @($outputDirectory, $temporaryOutputDirectory)
 
+	$publishedNUnitTestsDirectory = "$temporaryOutputDirectory\_PublishedNUnitTests"
+	$publishedxUnitTestsDirectory = "$temporaryOutputDirectory\_PublishedxUnitTests"
+	$publishedMSTestTestsDirectory = "$temporaryOutputDirectory\_PublishedMSTestTests"
+
+	$testResultsDirectory = "$outputDirectory\TestResults"
+	$NUnitTestResultsDirectory = "$testResultsDirectory\NUnit"
+	$xUnitTestResultsDirectory = "$testResultsDirectory\xUnit"
+	$MSTestTestResultsDirectory = "$testResultsDirectory\MSTest"
+
 	$buildConfiguration = "Release"
 	$buildPlatform = "Any CPU"
+
+	$packagesPath = "$solutionDirectory\packages"
+	$NUnitExe = (Find-PackagePath $packagesPath "NUnit.Runners" ) + "\Tools\nunit-console-x86.exe"
+	$xUnitExe = (Find-PackagePath $packagesPath "xUnit.Runner.Console" ) + "\Tools\xunit.console.exe"
+	$vsTestExe = (Get-ChildItem ("C:\Program Files (x86)\Microsoft Visual Studio*\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe")).FullName | Sort-Object $_ | select -last 1
 }
 
 FormatTaskName "`r`n`r`n--------Executing {0} Task ---------"
@@ -44,6 +60,58 @@ task Compile -depends Init `
 	}
 }
 
-task Test -depends Compile, Clean {
-	Write-Host $testMessage
+task TestNUnit `
+	-depends Compile `
+	-description "Run NUnit tests" `
+	-precondition { return Test-Path $publishedNUnitTestsDirectory } `
+	-requiredVariable publishedNUnitTestsDirectory, NUnitTestResultsDirectory `
+{
+	$testAssemblies = Prepare-Tests -testRunnerName "NUnit" `
+									-publishedTestsDirectory $publishedNUnitTestsDirectory `
+									-testResultsDirectory $NUnitTestResultsDirectory
+
+	Exec { &$nunitExe $testAssemblies /xml:$NUnitTestResultsDirectory\NUnit.xml /nologo /noshadow }
+}
+
+task TestXUnit `
+	-depends Compile `
+	-description "Run xUnit tests" `
+	-precondition { return Test-Path $publishedxUnitTestsDirectory } `
+	-requiredVariable publishedxUnitTestsDirectory, xUnitTestResultsDirectory `
+{
+	$testAssemblies = Prepare-Tests -testRunnerName "xUnit" `
+									-publishedTestsDirectory $publishedxUnitTestsDirectory `
+									-testResultsDirectory $xUnitTestResultsDirectory
+
+	Exec { &$xUnitExe $testAssemblies -xml $xUnitTestResultsDirectory\xUnit.xml -nologo -noshadow }
+}
+
+task TestMSTest `
+	-depends Compile `
+	-description "Run MSTest tests" `
+	-precondition { return Test-Path $publishedMSTestTestsDirectory } `
+	-requiredVariable publishedMSTestTestsDirectory, MSTestTestResultsDirectory `
+{
+	$testAssemblies = Prepare-Tests -testRunnerName "MSTest" `
+									-publishedTestsDirectory $publishedMSTestTestsDirectory `
+									-testResultsDirectory $MSTestTestResultsDirectory
+
+	# vstest console doesn't have any option to change the output directory
+	# so we need to change the working directory
+	Push-Location $MSTestTestResultsDirectory
+	#TODO: make category configurable
+	Exec { &$vsTestExe $testAssemblies /Logger:trx /TestCaseFilter:TestCategory=UnitTest}
+	Pop-Location
+
+	# move the .trx file back to $MSTestTestResultsDirectory
+	Move-Item -path $MSTestTestResultsDirectory\TestResults\*.trx -destination $MSTestTestResultsDirectory\MSTest.trx
+
+	Remove-Item $MSTestTestResultsDirectory\TestResults
+}
+
+task Test `
+    -depends Compile, TestNUnit, TestXUnit, TestMSTest  `
+	-description "Run unit tests"
+{
+  Write-Host $testMessage
 }
---
+++ Setup/helpers.ps1
@@ -0,0 +1,32 @@
+﻿function Find-PackagePath {
+    [CmdletBinding()]
+    param ( 
+        [Parameter(Position=0, Mandatory=1)]$packagesPath,
+        [Parameter(Position=1, Mandatory=1)]$packageName
+    )
+
+    return (Get-ChildItem($packagesPath + "\" + $packageName + "*")).FullName | Sort-Object $_ | select -Last 1
+}
+
+function Prepare-Tests {
+	[CmdletBinding()]	param(		[Parameter(Position=0,Mandatory=1)]$testRunnerName,		[Parameter(Position=1,Mandatory=1)]$publishedTestsDirectory,		[Parameter(Position=2,Mandatory=1)]$testResultsDirectory	)
+
+	$projects = Get-ChildItem $publishedTestsDirectory
+
+	if ($projects.Count -eq 1) {
+		Write-Host "1 $testRunnerName project has been found:"
+	}
+	else {
+		Write-Host $projects.Count " $testRunnerName projects have been found:"
+	}
+	
+	Write-Host ($projects | Select $_.Name )
+
+	# Create the test results directory if needed	if (!(Test-Path $testResultsDirectory)) {		Write-Host "Creating test results directory located at $testResultsDirectory"		mkdir $testResultsDirectory | Out-Null	}
+
+	# Get the list of test DLLs	$testAssembliesPaths = $projects | ForEach-Object { $_.FullName + "\" + $_.Name + ".dll" }
+
+	$testAssemblies = [string]::Join(" ", $testAssembliesPaths)
+
+	return $testAssemblies
+}
