7629fdc28cc90897a949097d400c191bd695ffd8
Olawale Adeleye <olawale.adeleye@gmail.com>
2016-12-20 17:10:30 -0500

    added packaging

--- Setup/default.ps1
+++ Setup/default.ps1
@@ -6,6 +6,7 @@ properties {
 
 	$buildServer = 'teamcity'
 	$testCategory = 'Unit'
+	$packageType = 'zip'
 
 	$solutionDirectory = (Get-Item $solutionFile).DirectoryName
 	$outputDirectory = "$solutionDirectory\.build"
@@ -15,27 +16,34 @@ properties {
 	$publishedNUnitTestsDirectory = "$temporaryOutputDirectory\_PublishedNUnitTests"
 	$publishedxUnitTestsDirectory = "$temporaryOutputDirectory\_PublishedxUnitTests"
 	$publishedMSTestTestsDirectory = "$temporaryOutputDirectory\_PublishedMSTestTests"
+	$publishedApplicationsDirectory = "$temporaryOutputDirectory\_PublishedApplications"
+	$publishedWebApplicationsDirectory = "$temporaryOutputDirectory\_PublishedWebsites"
 
 	$testResultsDirectory = "$outputDirectory\TestResults"
 	$NUnitTestResultsDirectory = "$testResultsDirectory\NUnit"
 	$xUnitTestResultsDirectory = "$testResultsDirectory\xUnit"
 	$MSTestTestResultsDirectory = "$testResultsDirectory\MSTest"
+	
 
 	$testCoverageDirectory = "$outputDirectory\TestCoverage"
 	$testCoverageReportPath = "$testCoverageDirectory\OpenCover.xml"
 	$testCoverageFilter = "+[*]* -[xunit.*]* -[*.NUnitTests]* -[*.Tests]* -[*.xUnitTests]*"
 	$testCoverageExcludeByAttribute = "*.ExcludeFromCodeCoverage*"
 	$testCoverageExcludeByFile = "*\*Designer.cs; *\*.g.cs; *\*.g.i.cs"
+	$packagesOutputDirectory = "$outputDirectory\Packages"
+	$applicationsOutputDirectory = "$packagesOutputDirectory\Applications"
 
 	$buildConfiguration = "Release"
 	$buildPlatform = "Any CPU"
 
 	$packagesPath = "$solutionDirectory\packages"
-	$NUnitExe = (Find-PackagePath $packagesPath "NUnit.Runners" ) + "\Tools\nunit-console-x86.exe"
+	$NUnitExe = (Find-PackagePath $packagesPath "NUnit.ConsoleRunner" ) + "\Tools\nunit3-console.exe"
 	$xUnitExe = (Find-PackagePath $packagesPath "xUnit.Runner.Console" ) + "\Tools\xunit.console.exe"
 	$vsTestExe = (Get-ChildItem ("C:\Program Files (x86)\Microsoft Visual Studio*\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe")).FullName | Sort-Object $_ | select -last 1
 	$openCoverExe = (Find-PackagePath $packagesPath "OpenCover") + "\Tools\OpenCover.Console.exe"
 	$reportGeneratorExe = (Find-PackagePath $packagesPath "ReportGenerator") + "\Tools\ReportGenerator.exe"
+	$7ZipExe = (Find-PackagePath $packagesPath "7-Zip.CommandLine") + "\Tools\7za.exe"
+	$nugetExe = (Find-PackagePath $packagesPath "NuGet.CommandLine") + "\Tools\NuGet.exe"
 }
 
 FormatTaskName "`r`n`r`n--------Executing {0} Task ---------"
@@ -45,6 +53,17 @@ task default -depends Test
 task Init -description "Initialises the build by removing previous artifacts and creating output directories" -requiredVariables $newDirectoryList {
 	Assert -conditionToCheck ("Debug", "Release" -contains $buildConfiguration) -failureMessage "Invalid build configuration '$buildConfiguration'. Valid values are 'Debug' or 'Release'"
 	Assert -conditionToCheck ("x64", "x86", "Any CPU" -contains $buildPlatform) -failureMessage "Invalid build platform '$buildPlatform'. Valid values are 'x64' , 'x86' or 'Any CPU'"
+
+	# Check that all tools are available
+	Write-Host "Checking that all required tools are available"
+ 
+	Assert (Test-Path $NUnitExe) "NUnit Console could not be found"
+	Assert (Test-Path $xUnitExe) "xUnit Console could not be found"
+	Assert (Test-Path $vsTestExe) "VSTest Console could not be found"
+	Assert (Test-Path $openCoverExe) "OpenCover Console could not be found"
+	Assert (Test-Path $reportGeneratorExe) "ReportGenerator Console could not be found"
+	Assert (Test-Path $7ZipExe) "7-Zip Command Line could not be found"
+	Assert (Test-Path $nugetExe) "NuGet Command Line could not be found"
 
 	foreach ($directory in $newDirectoryList) {
 		if (Test-Path $directory) {
@@ -153,6 +172,47 @@ task TestMSTest `
 
 	Remove-Item $MSTestTestResultsDirectory\TestResults
 }
+
+task Package `
+	-description "Packages all the applications" `
+	-depends Compile, Test `
+	-requiredVariables publishedApplicationsDirectory, publishedWebApplicationsDirectory, applicationsOutputDirectory {
+	$applications = @()
+
+	if(Test-Path $publishedApplicationsDirectory) {
+		$applications += @(Get-ChildItem $publishedApplicationsDirectory)
+	}
+
+	if(Test-Path $publishedWebApplicationsDirectory) {
+		$applications += @(Get-ChildItem $publishedWebApplicationsDirectory)
+	}
+
+	if ($applications.Length -gt 0 -and !(Test-Path $applicationsOutputDirectory)) {
+		New-Item $applicationsOutputDirectory -ItemType Directory | Out-Null
+	}
+
+	foreach ($application in $applications) {
+		if ($packageType -eq "zip") {
+			Write-Host "Packaging $($application.Name) as zip file"
+
+			$archivePath = "$applicationsOutputDirectory\$($application.Name).zip"
+			$inputDirectory = "$($application.FullName)\*"
+			Exec { &$7ZipExe a -r -mx3 $archivePath $inputDirectory }
+		}
+
+		if ($packageType -eq "nuget") {
+			$projectFile = (Get-ChildItem($solutionDirectory + "\*\" + $applications.Name + ".csproj")).FullName | select -First 1
+			Write-Host "File Here: $projectFile"
+
+			if($projectFile) {
+				Write-Host "Packaging $($application.Name) as NuGet package"
+
+				Exec { &$nugetExe pack "$projectFile" -Build -Properties Configuration=$buildConfiguration -IncludeReferencedProjects -OutputDirectory $applicationsOutputDirectory }
+			}
+		}
+	}
+}
+
 
 task Test `
 	-depends Compile, TestNUnit, TestXUnit, TestMSTest `
--- build.ps1
+++ build.ps1
@@ -1,4 +1,8 @@
-﻿cls
+﻿params (
+	[Int32]$buildNumber=0
+)
+
+cls
 
 # '[p]sake' is the same as 'psake' but $Error is not polluted
 Remove-Module [p]sake
@@ -8,14 +12,17 @@ $psakeModule = (Get-ChildItem (".\packages\psake*\tools\psake.psm1")).FullName |
 
 Import-Module $psakeModule
 
-Invoke-psake -buildFile .\Setup\default.ps1 -taskList Test `
+Invoke-psake -buildFile .\Setup\default.ps1 -taskList Package `
 	-framework 4.6.1 `
 	-properties @{ 
-		"buildServer" = "jenkins"
+		"buildServer" = "jenkins" #or teamcity
 		"testCategory" = "UnitTest"
+		"packageType" = "nuget" #or zip
 		"buildConfiguration" = "Release" 
 		"buildPlatform" = "Any CPU"} `
-	-parameters @{ "solutionFile" = "..\EmployeeManagement.sln"}
+	-parameters @{ 
+		"solutionFile" = "..\EmployeeManagement.sln"
+		"buildNumber" = $buildNumber}
 
 Write-Host "Build exit code:" $LASTEXITCODE
 
